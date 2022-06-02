a33d9851916fcf1910d44a2e18ad5f7ca464e080
Fandi <98140028+Camilo241@users.noreply.github.com>
2022-06-02 13:30:35 -0500

    Delete Taller_listas directory

--- Taller_listas/1.py
+++
@@ -1,7 +0,0 @@
-paises = list()
-num_paises = int(input('Ingrese la cantidad de paises a ingresar: '))
-for i in range(0,num_paises):
-    pais=input(f"num_paises {i+1}: ")
-    paises.append(pais)
-    paises.sort()
-print(paises)
